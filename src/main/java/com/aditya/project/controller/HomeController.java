f8916c8fd9ed38f5d7e8443ac3073cb4ec6a0c34
AdityaKshettri <adikshettri1623@gmail.com>
2021-03-24 18:19:47 +0530

    refactored code

--- src/main/java/com/aditya/project/controller/HomeController.java
+++ src/main/java/com/aditya/project/controller/HomeController.java
@@ -2,12 +2,10 @@ package com.aditya.project.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Locale;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,7 +15,7 @@ public class HomeController {
     private final MessageSource messageSource;
 
     @GetMapping("")
-    public String home(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
-        return messageSource.getMessage("good.morning.message", null, locale);
+    public String home() {
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 }
